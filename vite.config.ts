0902db198a630a2ab4425d956aa70df99c8f7510
alhamdul1ll4h <406659039@yru.ac.th>
2026-07-15 22:09:39 +0700

    Set base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/sanctuary-school/', // <-- เพิ่มบรรทัดนี้ลงไป
   server: {
     host: "::",
     port: 8080,
